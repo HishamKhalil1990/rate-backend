4e48e70274de132a0d404dc4330b262c101fcedd
hishamAbuodeh <H.Ibraheem@abuodeh.com>
2022-07-23 11:17:30 +0300

    done

--- data/forms.sql
+++ data/forms.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS formstable;
 CREATE TABLE formstable(
     id SERIAL PRIMARY KEY,
     warehouse VARCHAR(255) not null,
-    visit TIMESTAMP,
+    visit VARCHAR(255) not null,
     answers VARCHAR(255) not null,
     note text,
     username VARCHAR(255) not null
