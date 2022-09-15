a2b98e96452461be7a193b31dffc6d6958b6a268
hishamAbuodeh <H.Ibraheem@abuodeh.com>
2022-09-15 08:37:06 +0300

    edit in schemas

--- data/seed.sql
+++ data/seed.sql
@@ -1,2 +1,2 @@
-INSERT INTO userstable (username,pass) VALUES ('hisham','hisham')
-INSERT INTO supervisorUsertable (username,pass,cardcode) VALUES ('hisham','hisham',"C0000075")
+INSERT INTO userstable (username,pass) VALUES ('hisham','hisham');
+INSERT INTO supervisorUsertable (username,pass,cardcode) VALUES ('hisham','hisham','C0000075')
--- data/supervisorUser.sql
+++ data/supervisorUser.sql
@@ -2,6 +2,6 @@ DROP TABLE IF EXISTS supervisorUsertable;
 CREATE TABLE supervisorUsertable(
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) not null,
-    pass VARCHAR(255) not null
+    pass VARCHAR(255) not null,
     cardcode VARCHAR(255) not null
 );
